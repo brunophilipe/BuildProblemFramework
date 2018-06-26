6c09810b98f415c378f160a5434c064eaf80e8a6
Bruno Philipe <git@bruno.ph>
2018-06-26 22:11:04 +0200

    Initial Commit

---
+++ BuildProblemFramework/BuildProblemFramework.h
@@ -0,0 +1,19 @@
+//
+//  BuildProblemFramework.h
+//  BuildProblemFramework
+//
+//  Created by Bruno Philipe on 26/6/18.
+//  Copyright © 2018 Bruno Philipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BuildProblemFramework.
+FOUNDATION_EXPORT double BuildProblemFrameworkVersionNumber;
+
+//! Project version string for BuildProblemFramework.
+FOUNDATION_EXPORT const unsigned char BuildProblemFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuildProblemFramework/PublicHeader.h>
+
+
